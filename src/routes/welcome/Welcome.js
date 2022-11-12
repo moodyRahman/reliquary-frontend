f4b9a43b969485649d4c396b8e5effe33710bee9
Moody Rahman <moody.byte@gmail.com>
2022-11-11 19:49:35 -0500

    Welcome Pages

---
+++ src/routes/welcome/Welcome.js
@@ -0,0 +1,17 @@
+import Page from "../../components/Page";
+import "./Welcome.css"
+
+const Welcome = () => {
+  return (
+    <Page>
+      <div className="logo">
+        Reliquary
+      </div>
+      <div>
+        Login
+      </div>
+    </Page>
+  );
+}
+
+export default Welcome;

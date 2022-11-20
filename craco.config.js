03adc11a2dd27829fd5b01fde72462a9c470c721
Moody Rahman <moody.byte@gmail.com>
2022-11-20 09:27:40 -0500

    craco

--- craco.config.js
+++ craco.config.js
@@ -3,7 +3,23 @@ const path = require('path');
 module.exports = {
   webpack: {
     alias: {
-      "@routes": path.resolve(__dirname, 'src/routes/')
-    }
+      "@routes": path.resolve(__dirname, 'src/routes/'),
+      "@components":path.resolve(__dirname, "src/components")
+    },
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index_bundle.js',
+      publicPath: '/'
+    },
+    module: {
+      rules: [
+        { test: /\.(js)$/, use: 'babel-loader' },
+        { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+      ]
+    },
+    devServer: {
+      historyApiFallback: true,
+    },
   }
 };

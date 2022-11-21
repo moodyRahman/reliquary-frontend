08280f7ea4a6c88f1742560b5b4df8fc8c8f854c
Moody Rahman <moody.byte@gmail.com>
2022-11-20 19:11:48 -0500

    delete captcha badge, add captchainfo component

--- src/components/Captcha.tsx
+++ src/components/Captcha.tsx
@@ -1,39 +1,53 @@
-import { MouseEvent } from "react"
+import { MouseEvent } from "react";
 
 declare global {
-    interface Window {
-      grecaptcha: ReCaptchaInstance
-      captchaOnLoad: () => void
-    }
+  interface Window {
+    grecaptcha: ReCaptchaInstance;
+    captchaOnLoad: () => void;
   }
-  
-  interface ReCaptchaInstance {
-    ready: (cb: () => any) => void
-    execute: (public_key: string, options: ReCaptchaExecuteOptions) => Promise<string>
-    render: (id: string, options: ReCaptchaRenderOptions) => any
-  }
-  
-  interface ReCaptchaExecuteOptions {
-    action: string
-  }
-  
-  interface ReCaptchaRenderOptions {
-    sitekey: string
-    size: 'invisible'
-  }
-  
-
-const captcha = (func: Function) => {
-    return (e:MouseEvent) => {
-        e.preventDefault()
-        window.grecaptcha.ready(() => {
-            window.grecaptcha
-                .execute("6LfAwBAjAAAAAALoD-IU1Qt_qeUNXMxFmjSosm2k", { action: "register" })
-                .then(token => {
-                    func(token)
-                })
-        })
-    }
 }
 
-export default captcha
+interface ReCaptchaInstance {
+  ready: (cb: () => any) => void;
+  execute: (
+    public_key: string,
+    options: ReCaptchaExecuteOptions
+  ) => Promise<string>;
+  render: (id: string, options: ReCaptchaRenderOptions) => any;
+}
+
+interface ReCaptchaExecuteOptions {
+  action: string;
+}
+
+interface ReCaptchaRenderOptions {
+  sitekey: string;
+  size: "invisible";
+}
+
+const captcha = (func: Function) => {
+  return (e: MouseEvent) => {
+    e.preventDefault();
+    window.grecaptcha.ready(() => {
+      window.grecaptcha
+        .execute("6LfAwBAjAAAAAALoD-IU1Qt_qeUNXMxFmjSosm2k", {
+          action: "register",
+        })
+        .then((token) => {
+          func(token);
+        });
+    });
+  };
+};
+
+const CaptchaInfo = () => {
+  return (
+    <div className="captcha-info">
+      This site is protected by reCAPTCHA and the Google{" "}
+      <a href="https://policies.google.com/privacy">Privacy Policy</a> and{" "}
+      <a href="https://policies.google.com/terms">Terms of Service</a> apply.
+    </div>
+  );
+};
+
+export { captcha, CaptchaInfo };

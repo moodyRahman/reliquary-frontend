06e530b76e7f807e4856c207afd4c25b0cf36b4e
Moody Rahman <moody.byte@gmail.com>
2022-12-03 17:12:45 -0500

    character creation form

--- src/routes/characters/Characters.js
+++ src/routes/characters/Characters.js
@@ -1,8 +1,16 @@
+import { Form } from "react-router-dom"
+import { useState } from "react";
 
 const Characters = () => {
-    return(
+
+    const [setName, name] = useState("");
+    const [setDescription, description] = useState("")
+
+    return (
         <div>
-            characters :0
+            <input type="text" placeholder="name" name="name" onChange={(e) => setName(e.target.value)} />
+            <input type="text" placeholder="description" name="description" onChange={(e) => setDescription(e.target.value)} />
+            <button onClick={(e) => { console.log(e) }}>login</button>
         </div>
     )
 }
